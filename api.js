ba06986535e7251129f0b94b94058da57bd70b30
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-08-31 15:27:07 -0500

    finally able to get fetch to retrieve the data in the response and print to console for later use

--- api.js
+++ api.js
@@ -1,3 +1,3 @@
-const apiKey = "HelloD2zzF5f3FqQS2kO98BqGe2CzzaM9DNmm";
+const apiKey = "D2zzF5f3FqQS2kO98BqGe2CzzaM9DNmm";
 
 export {apiKey};
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import { apiKey } from "./api.js";
 
-
 const submitBTN = document.getElementById("submitBTN");
 
-function newGif () {
-    const loadingWheel = document.querySelector(".loadingWheel");
-    loadingWheel.style.display = "block";
+async function newGif() {
+  const loadingWheel = document.querySelector(".loadingWheel");
+  loadingWheel.style.display = "block";
 
-};
+  const searchResults = document.getElementById("searchBox").value;
+  console.log(searchResults);
 
+  const limit = 10;
 
+  try {
+    const results = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchResults}&limit=${limit}&offset=0&rating=g&lang=en`
+    );
 
+    const data = await results.json();
+    console.log(data.data);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    loadingWheel.style.display = "none";
+  }
+}
+
+newGif();
 submitBTN.addEventListener("click", newGif);
