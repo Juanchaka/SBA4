982ef21f06a73da3780bcc4f3a2cb869019168e5
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-08-31 23:24:14 -0500

    added some basiv error handling and spliting of the comma separated list to send a proper array of movies

--- api.js
+++ api.js
@@ -3,15 +3,21 @@ const apiKey = "D2zzF5f3FqQS2kO98BqGe2CzzaM9DNmm";
 const postBTN = document.getElementById("POST");
 
 async function response() {
-
   alert(
     "You will now use the POST feature to add an actor and their movies to a database. Press 'OK' to continue."
   );
+
   let actorFirstName = prompt("Please enter an actor's first name");
   let actorLastName = prompt("Please enter an actor's last name");
   let actorMovies = prompt(
     "Please enter a comma separated list of the actor's movies"
   );
+  if (!actorFirstName || !actorLastName || !actorMovies) {
+    alert("Request not submitted! Please fill in all fields.");
+    return;
+  };
+
+  const moviesArray = actorMovies.split(',').map(movie => movie.trim());
 
   const response = await fetch("https://reqres.in/api/users?page=2", {
     method: "POST",
@@ -20,10 +26,10 @@ async function response() {
     },
     body: JSON.stringify({
       name: {
-        firstName: `${actorFirstName}`,
-        lastName: `${actorLastName}`,
+        firstName: actorFirstName,
+        lastName: actorLastName,
       },
-      movies: [`${actorMovies}`],
+      movies: moviesArray
     }),
   });
   const that = await response.json();
@@ -36,16 +42,16 @@ async function response() {
     } has starred in: ${that.movies.map((movie) => movie).join(", ")}
     `
   );
-//   console.log(`You successfully POSTed at ${that.createdAt}`);
-//   console.log(`Your actor's user ID is ${that.id}`);
-//   console.log(
-//     `Your actor's full name is ${that.name.firstName} ${that.name.lastName}`
-//   );
-//   console.log(
-//     `The movies ${that.name.firstName} ${
-//       that.name.lastName
-//     } has starred in: ${that.movies.map((movie) => movie).join(", ")}`
-//   );
+  //   console.log(`You successfully POSTed at ${that.createdAt}`);
+  //   console.log(`Your actor's user ID is ${that.id}`);
+  //   console.log(
+  //     `Your actor's full name is ${that.name.firstName} ${that.name.lastName}`
+  //   );
+  //   console.log(
+  //     `The movies ${that.name.firstName} ${
+  //       that.name.lastName
+  //     } has starred in: ${that.movies.map((movie) => movie).join(", ")}`
+  //   );
 
   //   const test = await fetch("https://reqres.in/api/users?page=2");
   //   if (!test.ok) {
